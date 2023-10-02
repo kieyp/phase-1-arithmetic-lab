2bdb2e9bac0d110ca6a78afddce63f71bbd6ef38
Boniface <Cheruiyotbony@gmail.com>
2023-10-02 03:40:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+
+let multiply=0;
+let num1=2
+let num2=31
+
+multiply=num1*num2
+console.log(multiply)
+
+let random=Math.floor(Math.random() *10 )+1;
+
+let num3=16
+let num4=6
+
+let mod=num3%num4
+console.log(mod)
+
+let list=(1,2,3,4,5,6,7,20)
+let max=Math.min(list);
+
+console.log(max)
